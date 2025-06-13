6cef6b1e630466da5ef9189f00a1ecc4addec557
devz <evankrauss1209@gmail.com>
2025-06-12 21:38:24 -0400

    Three.js rendering test

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { FileDialog } from "./file-dialog.js";
+import * as Canvas from "./canvas.js";
 
 const open_button /* HTMLButtonElement */ = document.getElementById("open-button");
 
@@ -10,3 +11,19 @@ open_button.onclick = function() {
     var file_dialog /* FileDialog */ = new FileDialog(".gsc");
     file_dialog.open();
 }
+
+export const canvas = new Canvas.CanvasRenderer("render-window");
+
+
+const box = new Canvas.Box(1, 1, 1, 0xffffff);
+canvas.add(box.mesh);
+
+const light = new Canvas.HemisphereLight(0xffffff, 0x000000, 5);
+canvas.add(light.light);
+
+canvas.renderer.setAnimationLoop(function() {
+    box.mesh.rotation.x += 0.01;
+    box.mesh.rotation.y += 0.01;
+
+    canvas.render();
+});
